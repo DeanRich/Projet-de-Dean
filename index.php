5aa1a41a6d8a7114d0fe4351a2d0a3781fa5d79b
DeanRich <45569572+DeanRich@users.noreply.github.com>
2018-12-23 16:59:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<div id="container">
+<div id="text1">
+<style type="text/css">
+ body { 
+ background-color:white;
+background-image:url(Cybersecurity-2.jpg);
+ background-repeat:no-repeat; 
+ } 
+
+ h1{
+  color: yellow; /*Colorer le grand titre en ; Jaune*/
+}
+
+ a{
+  color: white;
+ }
+
+
+ </style>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog : Avicyber</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Blog : Avicyber</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="article.php">Ajouter commentaire</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+
+    <br><br><br>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <center><img src="avicyber logo.png"></center>
+          <b><h1 class="mt-5"><font color="Yellow">Avicyber</font></h1>
+          <b><font color="yellow" size="9"><p class="lead">Bienvenue dans mon blog</p></font>
+          <ul class="list-unstyled">
+           <a href="article.php"><li><font color="white">Vous pouvez laisser vos commentaires ici </a></li></font></a>
+          </ul>
+          
+  
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+   
+    <br><br><br><br>
+    <br><br><br><br><br><br><font color="white">Copyright © 2018 Dean RICHARDS.All Rights Reserved.</font>
+  </body>
+
+</html>
